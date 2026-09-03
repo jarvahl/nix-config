dd8d7f47db72d3bead88c10b04d2d6ef0030dace
declnix <20567435+declnix@users.noreply.github.com>
2026-09-03 23:33:58 +0200

    terra: hyprland -> split foot and swaybg

---
+++ modules/den/+hosts/x86_64-linux/+terra/aspects/+provides/jarvahl/foot.nix
@@ -0,0 +1,16 @@
+{ ... }:
+{
+  den.aspects.terra = {
+    provides.jarvahl = {
+      hjem = { pkgs, ... }: {
+        files.".config/foot/foot.ini".text = ''
+          [main]
+          pad=16x16
+          font=monospace:size=12
+        '';
+
+        packages = [ pkgs.foot ];
+      };
+    };
+  };
+}
