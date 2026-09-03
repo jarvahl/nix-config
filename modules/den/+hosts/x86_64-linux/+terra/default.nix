66246ea7e0cb9dcddbdf7e39fceaec0c83cff75c
declnix <20567435+declnix@users.noreply.github.com>
2026-09-04 01:09:42 +0200

    terra: remove nixosContainers services

--- modules/den/+hosts/x86_64-linux/+terra/default.nix
+++ modules/den/+hosts/x86_64-linux/+terra/default.nix
@@ -1,32 +1,5 @@
 { den, ... }:
 {
-  den.aspects.terra.nixos = {
-    containers.titan = {
-      ephemeral = true;
-      bindMounts = {
-        "/var/lib/private/n8n" = {
-          hostPath = "/var/lib/titan/n8n";
-          isReadOnly = false;
-        };
-        "/var/lib/private/ollama" = {
-          hostPath = "/var/lib/titan/ollama";
-          isReadOnly = false;
-        };
-        "/var/lib/private/open-webui" = {
-          hostPath = "/var/lib/titan/open-webui";
-          isReadOnly = false;
-        };
-      };
-    };
-
-    systemd.tmpfiles.rules = [
-      "d /var/lib/titan 0755 root root -"
-      "d /var/lib/titan/n8n 0755 root root -"
-      "d /var/lib/titan/ollama 0755 root root -"
-      "d /var/lib/titan/open-webui 0755 root root -"
-    ];
-  };
-
   den.aspects.terra = {
     includes =
       (with den.aspects; [
@@ -40,6 +13,5 @@
 
   den.hosts.x86_64-linux.terra = {
     users.jarvahl = { };
-    nixosContainers = [ den.hosts.x86_64-linux.titan ];
   };
 }
