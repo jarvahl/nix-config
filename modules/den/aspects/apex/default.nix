273b778184df10bf2139b3e73935e766dc048056
jarvahl <20567435+jarvahl@users.noreply.github.com>
2026-09-09 14:29:23 +0200

    apex: grafana -> add service

--- modules/den/aspects/apex/default.nix
+++ modules/den/aspects/apex/default.nix
@@ -43,6 +43,18 @@
       includes = [ (den.batteries.unfree [ "n8n" ]) ];
     }
     {
+      nixos =
+        { config, ... }:
+        {
+          services.grafana = {
+            enable = true;
+            settings.security.secret_key = "$__file{${config.sops.secrets."grafana/secret-key".path}}";
+          };
+
+          sops.secrets."grafana/secret-key".owner = "grafana";
+        };
+    }
+    {
       nixos = { pkgs, ... }: {
         services.ollama = {
           enable = true;
