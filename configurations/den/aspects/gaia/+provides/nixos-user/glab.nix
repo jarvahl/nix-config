dccd6bc8c73003a257f6775114fd689113c211b9
declnix <20567435+declnix@users.noreply.github.com>
2026-09-15 02:19:49 +0200

    gaia(nixos-user): gh + glab -> configure secret-backed Git identities

--- configurations/den/aspects/gaia/+provides/nixos-user/glab.nix
+++ configurations/den/aspects/gaia/+provides/nixos-user/glab.nix
@@ -1,6 +1,38 @@
 { ... }:
 {
-  den.aspects.gaia.provides.nixos-user.hjem = { pkgs, ... }: {
-    packages = [ pkgs.glab ];
+  den.aspects.gaia.provides.nixos-user = {
+    hjem = { pkgs, ... }: {
+      packages = [ pkgs.glab ];
+    };
+
+    nixos = { config, ... }: {
+      sops.secrets = {
+        "users/nixos/gitlab/username" = { };
+        "users/nixos/gitlab/email" = { };
+      };
+
+      sops.templates."gitlab-identity".content = ''
+        [user]
+            name = ${config.sops.placeholder."users/nixos/gitlab/username"}
+            email = ${config.sops.placeholder."users/nixos/gitlab/email"}
+      '';
+
+      sops.templates."git-identity-includes".content = ''
+        [includeIf "hasconfig:remote.*.url:https://github.com/"]
+            path = ~/.config/git/github-identity
+
+        [includeIf "hasconfig:remote.*.url:https://${config.sops.placeholder."gitlab/host"}/"]
+            path = ~/.config/git/gitlab-identity
+      '';
+
+      hjem.users.nixos = {
+        files = {
+          ".config/git/gitlab-identity".source = config.sops.templates."gitlab-identity".path;
+          ".config/git/git-identity-includes".source = config.sops.templates."git-identity-includes".path;
+        };
+
+        rum.programs.git.settings.include.path = "~/.config/git/git-identity-includes";
+      };
+    };
   };
 }
