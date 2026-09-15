51dd58d90a300d7529fd2014e6f12ce596d8cb5e
declnix <20567435+declnix@users.noreply.github.com>
2026-09-15 02:19:48 +0200

    apex(jarvahl): gh -> configure secret-backed GitHub identity

--- configurations/den/aspects/apex/+provides/jarvahl/gh.nix
+++ configurations/den/aspects/apex/+provides/jarvahl/gh.nix
@@ -1,6 +1,35 @@
 { ... }:
 {
-  den.aspects.apex.provides.jarvahl.hjem = { pkgs, ... }: {
-    packages = [ pkgs.gh ];
+  den.aspects.apex.provides.jarvahl = {
+    hjem = { pkgs, ... }: {
+      packages = [ pkgs.gh ];
+    };
+
+    nixos = { config, ... }: {
+      sops.secrets = {
+        "users/jarvahl/github/username" = { };
+        "users/jarvahl/github/email" = { };
+      };
+
+      sops.templates."github-identity".content = ''
+        [user]
+            name = ${config.sops.placeholder."users/jarvahl/github/username"}
+            email = ${config.sops.placeholder."users/jarvahl/github/email"}
+      '';
+
+      sops.templates."github-include".content = ''
+        [includeIf "hasconfig:remote.*.url:https://github.com/"]
+            path = ~/.config/git/github-identity
+      '';
+
+      hjem.users.jarvahl = {
+        files = {
+          ".config/git/github-identity".source = config.sops.templates."github-identity".path;
+          ".config/git/github-include".source = config.sops.templates."github-include".path;
+        };
+
+        rum.programs.git.settings.include.path = "~/.config/git/github-include";
+      };
+    };
   };
 }
