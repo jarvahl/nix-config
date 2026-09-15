4a0241d282568022581ab84b8beff8edfec2fc38
declnix <20567435+declnix@users.noreply.github.com>
2026-09-15 02:19:48 +0200

    cargo(nixos-user): gh -> configure secret-backed GitHub identity

--- configurations/den/aspects/cargo/+provides/nixos-user/gh.nix
+++ configurations/den/aspects/cargo/+provides/nixos-user/gh.nix
@@ -1,6 +1,35 @@
 { ... }:
 {
-  den.aspects.cargo.provides.nixos-user.hjem = { pkgs, ... }: {
-    packages = [ pkgs.gh ];
+  den.aspects.cargo.provides.nixos-user = {
+    hjem = { pkgs, ... }: {
+      packages = [ pkgs.gh ];
+    };
+
+    nixos = { config, ... }: {
+      sops.secrets = {
+        "users/nixos/github/username" = { };
+        "users/nixos/github/email" = { };
+      };
+
+      sops.templates."github-identity".content = ''
+        [user]
+            name = ${config.sops.placeholder."users/nixos/github/username"}
+            email = ${config.sops.placeholder."users/nixos/github/email"}
+      '';
+
+      sops.templates."github-include".content = ''
+        [includeIf "hasconfig:remote.*.url:https://github.com/"]
+            path = ~/.config/git/github-identity
+      '';
+
+      hjem.users.nixos = {
+        files = {
+          ".config/git/github-identity".source = config.sops.templates."github-identity".path;
+          ".config/git/github-include".source = config.sops.templates."github-include".path;
+        };
+
+        rum.programs.git.settings.include.path = "~/.config/git/github-include";
+      };
+    };
   };
 }
