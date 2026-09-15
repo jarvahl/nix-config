d094160e1726709d0ed9f559f2554b20ebb8c637
declnix <20567435+declnix@users.noreply.github.com>
2026-09-15 17:07:37 +0200

    cargo(nixos): gh -> fix SOPS template permissions

--- configurations/den/aspects/cargo/+provides/nixos-user/gh.nix
+++ configurations/den/aspects/cargo/+provides/nixos-user/gh.nix
@@ -11,16 +11,24 @@
         "users/nixos/github/email" = { };
       };
 
-      sops.templates."github-identity".content = ''
-        [user]
-            name = ${config.sops.placeholder."users/nixos/github/username"}
-            email = ${config.sops.placeholder."users/nixos/github/email"}
-      '';
+      sops.templates."github-identity" = {
+        content = ''
+          [user]
+              name = ${config.sops.placeholder."users/nixos/github/username"}
+              email = ${config.sops.placeholder."users/nixos/github/email"}
+        '';
+        owner = "nixos";
+        mode = "0400";
+      };
 
-      sops.templates."github-include".content = ''
-        [includeIf "hasconfig:remote.*.url:https://github.com/"]
-            path = ~/.config/git/github-identity
-      '';
+      sops.templates."github-include" = {
+        content = ''
+          [includeIf "hasconfig:remote.*.url:https://github.com/"]
+              path = ~/.config/git/github-identity
+        '';
+        owner = "nixos";
+        mode = "0400";
+      };
 
       hjem.users.nixos = {
         files = {
