5a239917e16c9aea6435a69fb0c2f7cb2332082c
declnix <20567435+declnix@users.noreply.github.com>
2026-09-20 11:42:27 +0200

    apex(jarvahl): herdr -> configure Worktrunk plugin

--- configurations/den/aspects/apex/+provides/jarvahl/herdr.nix
+++ configurations/den/aspects/apex/+provides/jarvahl/herdr.nix
@@ -1,7 +1,23 @@
 { ... }:
 {
-  den.aspects.apex.provides.jarvahl.hjem.programs.herdr = {
-    enable = true;
-    integrations.pi.enable = true;
+  den.aspects.apex.provides.jarvahl.hjem = { pkgs, ... }: {
+    programs.herdr = {
+      enable = true;
+      integrations.pi.enable = true;
+      extraConfig = ''
+        [[keys.command]]
+        key = "prefix+shift+g"
+        type = "plugin_action"
+        command = "worktrunk.open"
+        description = "Worktree: switch or create"
+      '';
+    };
+
+    packages = [ pkgs.herdr-worktrunk ];
+
+    files.".local/state/herdr/plugins/worktrunk".source = pkgs.herdr-worktrunk;
+    files.".config/herdr/plugins/config/worktrunk/config.toml".text = ''
+      open_mode = "workspace"
+    '';
   };
 }
--- hjemModules/collection/programs/herdr.nix
+++ hjemModules/collection/programs/herdr.nix
@@ -1,12 +1,19 @@
 { config, lib, pkgs, ... }:
 let
   cfg = config.programs.herdr;
+
 in
 {
   options.programs.herdr = {
     enable = lib.mkEnableOption "Herdr";
 
     integrations.pi.enable = lib.mkEnableOption "Herdr integration for Pi";
+
+    extraConfig = lib.mkOption {
+      type = lib.types.lines;
+      default = "";
+      description = "Additional Herdr configuration.";
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -18,6 +25,8 @@ in
       [theme]
       name = "catppuccin"
 
+      ${cfg.extraConfig}
+
       [theme.custom]
       sidebar_bg = "#181825"
       panel_bg = "#181825"
@@ -25,5 +34,6 @@ in
 
     programs.pi.skills.herdr = lib.mkIf cfg.integrations.pi.enable
       "${pkgs.herdr.src}/skills/herdr";
+
   };
 }
