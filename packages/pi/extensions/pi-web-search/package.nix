1691fd976d248c3f0e6f4c35d309065ba3c390cf
declnix <20567435+declnix@users.noreply.github.com>
2026-09-20 13:34:01 +0200

    packages: pi-web-search -> add pinned extension

---
+++ packages/pi/extensions/pi-web-search/package.nix
@@ -0,0 +1,8 @@
+{ fetchFromGitHub }:
+
+fetchFromGitHub {
+  owner = "ttttmr";
+  repo = "pi-web-search";
+  rev = "d0b9d2ec60c31fa9b4b9b1e62513fb6496fc3cfa";
+  hash = "sha256-3Fdks56MaxMG6BXZwq+QXMLhI+eGqZxQVBnz8VDeAx4=";
+}
